48f9cbe590cd2f872fdc8feaaeaa9145092b526c
Pavel Zet <zetorama@gmail.com>
2020-04-20 03:50:20 -0700

    highlight machines

--- objects/robot.go
+++ objects/robot.go
@@ -103,3 +103,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "highlight"
+  type: "sprite"
+  data: "tile_set: \"/assets/lights.atlas\"\n"
+  "default_animation: \"light_mask_128\"\n"
+  "material: \"/assets/materials/light.material\"\n"
+  "blend_mode: BLEND_MODE_ADD\n"
+  ""
+  position {
+    x: 0.0
+    y: 3.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
