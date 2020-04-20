c778734babc8f8374f5d924a874791485cd80af1
Pavel Zet <zetorama@gmail.com>
2020-04-20 11:25:56 -0700

    add alien

--- objects/alien.go
+++ objects/alien.go
@@ -1,6 +1,6 @@
 components {
-  id: "hero"
-  component: "/objects/hero.script"
+  id: "alien"
+  component: "/objects/alien.script"
   position {
     x: 0.0
     y: 0.0
@@ -19,14 +19,13 @@ embedded_components {
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
   "mass: 0.0\n"
-  "friction: 0.1\n"
-  "restitution: 0.5\n"
-  "group: \"hero\"\n"
+  "friction: 0.0\n"
+  "restitution: 0.0\n"
+  "group: \"alien\"\n"
   "mask: \"wall\"\n"
   "mask: \"robot\"\n"
   "mask: \"beacon\"\n"
   "mask: \"machine\"\n"
-  "mask: \"detection\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
@@ -67,8 +66,8 @@ embedded_components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/assets/hero.atlas\"\n"
-  "default_animation: \"hero-idle-empty-front\"\n"
+  data: "tile_set: \"/assets/alien.atlas\"\n"
+  "default_animation: \"alien-idle-empty-front\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -88,13 +87,58 @@ embedded_components {
   id: "light"
   type: "sprite"
   data: "tile_set: \"/assets/lights.atlas\"\n"
-  "default_animation: \"light_mask_32\"\n"
+  "default_animation: \"light_mask_green_32\"\n"
   "material: \"/assets/materials/light.material\"\n"
   "blend_mode: BLEND_MODE_ADD\n"
   ""
   position {
     x: 0.0
     y: 3.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "scaner"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"detection\"\n"
+  "mask: \"hero\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 80.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
     z: 0.0
   }
   rotation {
