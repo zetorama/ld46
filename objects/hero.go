db571fe520eb4784648e7ed29fc888cc7007a907
Pavel Zet <zetorama@gmail.com>
2020-04-20 03:30:27 -0700

    add lightning

--- objects/hero.go
+++ objects/hero.go
@@ -83,3 +83,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "light"
+  type: "sprite"
+  data: "tile_set: \"/assets/lights.atlas\"\n"
+  "default_animation: \"light_mask_32\"\n"
+  "material: \"/assets/materials/light.material\"\n"
+  "blend_mode: BLEND_MODE_ADD\n"
+  ""
+  position {
+    x: 0.0
+    y: 3.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
